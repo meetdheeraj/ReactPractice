4955da628679fd29aa9a71ae8321bed0d353ef23
meetdheeraj <kalgutkar.dheeraj@gmail.com>
2017-07-20 19:42:49 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//create component
+
+const App = function{
+    return <div>Hi!!!</div>;
+}
+
+//attach above component after converting to HTML to the DOM
+React.render(App);
